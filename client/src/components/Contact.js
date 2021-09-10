d3d17b0f8ebcc8b151d77590695cceeb20e55209
Krupali2204 <71226293+Krupali2204@users.noreply.github.com>
2021-09-11 02:41:10 +0530

    home todu kariu and half con

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -2,11 +2,66 @@ import React from 'react'
 
 const Contact = () => {
     return (
-        <div>
-            <p>WELCOME</p>
-            <h1>WELCOME TO Contact US</h1>
-         </div>
+        <>
+            <div className="my-5">
+                <h1 className="text-center">Contact_us</h1>
+            </div>
+            <div className="container Contact_div">
+                <div className="row">
+                    <div className="col-md-6 col-10 mx-auto">
+                        <form>
+                            <form class="row g-3">
+                                <div class="col-md-6">
+                                    <label for="inputEmail4" class="form-label">Email</label>
+                                    <input type="email" class="form-control" id="inputEmail4"></input>
+                                </div>
+                                <div class="col-md-6">
+                                    <label for="inputPassword4" class="form-label">Password</label>
+                                    <input type="password" class="form-control" id="inputPassword4"></input>
+                                </div>
+                                <div class="col-12">
+                                    <label for="inputAddress" class="form-label">Address</label>
+                                    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St"></input>
+                                </div>
+                                <div class="col-12">
+                                    <label for="inputAddress2" class="form-label">Address 2</label>
+                                    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor"></input>
+                                </div>
+                                <div class="col-md-6">
+                                    <label for="inputCity" class="form-label">City</label>
+                                    <input type="text" class="form-control" id="inputCity"></input>
+                                </div>
+                                <div class="col-md-4">
+                                    <label for="inputState" class="form-label">State</label>
+                                    <select id="inputState" class="form-select">
+                                        <option selected>Choose...</option>
+                                        <option>...</option>
+                                    </select>
+                                </div>
+                                <div class="col-md-2">
+                                    <label for="inputZip" class="form-label">Zip</label>
+                                    <input type="text" class="form-control" id="inputZip"></input>
+                                </div>
+                                <div class="col-12">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" id="gridCheck"></input>
+                                        <label class="form-check-label" for="gridCheck">
+                                            Check me out
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <button type="submit" class="btn btn-primary">Sign in</button>
+                                </div>
+                            </form>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </>
+
     )
+
 }
 
 export default Contact
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,12 +1,40 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
+import web from "../images/login.svg"
 
 const Home = () => {
     return (
-        <div>
-            <p className="pt-5">WELCOME</p>
-            <h1>WELCOME TO E-MED STORE</h1>
-         </div>
-    )
+        <>
+            <section id="header" className="d-flex align-items-center">
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-10 mx-auto">
+                            <div className="row">
+                            <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex justify-content-center flex-column justify-content-center">
+                                <h1>
+                                    grow your business with <strong className="brand-name">Emed</strong>
+                                </h1>
+                                <h2 className="my-3">
+                                    we are team of talented developer making websites
+                                </h2>
+                                <div className="mt-3">
+                                    <NavLink to="/contact" className="btn-get-started">
+                                        Contact_us
+                                    </NavLink>
+                                </div>
+                            </div>
+                            <div className="col-lg-6 order-1 order-lg-2 header-img">
+                                <img src={web} className="img-fluid animated" alt="home images"/>
+                                </div>
+                                </div>
+
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
 }
 
 export default Home
