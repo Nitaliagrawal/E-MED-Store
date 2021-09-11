382d22d9376be134051b3ff7b3e38ab255162462
Krupali2204 <71226293+Krupali2204@users.noreply.github.com>
2021-09-12 02:15:49 +0530

    contactus page design without css

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,66 +1,100 @@
 import React from 'react'
 
 const Contact = () => {
-    return (
-        <>
-            <div className="my-5">
-                <h1 className="text-center">Contact_us</h1>
-            </div>
-            <div className="container Contact_div">
-                <div className="row">
-                    <div className="col-md-6 col-10 mx-auto">
-                        <form>
-                            <form class="row g-3">
-                                <div class="col-md-6">
-                                    <label for="inputEmail4" class="form-label">Email</label>
-                                    <input type="email" class="form-control" id="inputEmail4"></input>
-                                </div>
-                                <div class="col-md-6">
-                                    <label for="inputPassword4" class="form-label">Password</label>
-                                    <input type="password" class="form-control" id="inputPassword4"></input>
-                                </div>
-                                <div class="col-12">
-                                    <label for="inputAddress" class="form-label">Address</label>
-                                    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St"></input>
-                                </div>
-                                <div class="col-12">
-                                    <label for="inputAddress2" class="form-label">Address 2</label>
-                                    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor"></input>
-                                </div>
-                                <div class="col-md-6">
-                                    <label for="inputCity" class="form-label">City</label>
-                                    <input type="text" class="form-control" id="inputCity"></input>
-                                </div>
-                                <div class="col-md-4">
-                                    <label for="inputState" class="form-label">State</label>
-                                    <select id="inputState" class="form-select">
-                                        <option selected>Choose...</option>
-                                        <option>...</option>
-                                    </select>
-                                </div>
-                                <div class="col-md-2">
-                                    <label for="inputZip" class="form-label">Zip</label>
-                                    <input type="text" class="form-control" id="inputZip"></input>
-                                </div>
-                                <div class="col-12">
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" id="gridCheck"></input>
-                                        <label class="form-check-label" for="gridCheck">
-                                            Check me out
-                                        </label>
-                                    </div>
-                                </div>
-                                <div class="col-12">
-                                    <button type="submit" class="btn btn-primary">Sign in</button>
-                                </div>
-                            </form>
-                        </form>
-                    </div>
+  return (
+    <>
+      <div className="contact_info">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-lg-10 offset-lg-1 d-flex justify-content-between">
+              {/* phone number*/}
+              <div className="contact_info_item d-flex justify-content-start align-items-center">
+                <img src="" alt="phone" />
+                <div className="contact_info_contect">
+                  <div className="contact_info_title">
+                    phone
+                  </div>
+                  <div className="contact_info_title">
+                    +91 8849550613
+                  </div>
                 </div>
+              </div>
+              <div className="contact_info_item d-flex justify-content-start align-items-center">
+                <img src="" alt="Email" />
+                <div className="contact_info_contect">
+                  <div className="contact_info_title">
+                    Email
+                  </div>
+                  <div className="contact_info_title">
+                    abc@gmail.com
+                  </div>
+                </div>
+              </div>
+              <div className="contact_info_item d-flex justify-content-start align-items-center">
+                <img src="" alt="Address" />
+                <div className="contact_info_contect">
+                  <div className="contact_info_title">
+                    Address
+                  </div>
+                  <div className="contact_info_title">
+                    Gopal Vidyanagar, Tarsadi, Gujarat 394350
+                  </div>
+                </div>
+              </div>
             </div>
-        </>
+          </div>
 
-    )
+        </div>
+
+      </div>
+
+
+
+      <div className="contact_form">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-10 offset-lg-1">
+              <div className="contact_form_container py-5">
+                <div className="contact_form_title">
+                  Get in Touch
+                </div>
+                <form id="contact_form">
+                  <div className="contact_form_name d-flex justify-content-between align-items-between">
+                    <input type="text" id="contact_form_name"
+                      className="contact_form_name input field"
+                      placeholder="Your Name" required="true" />
+
+                    <input type="Email" id="contact_form_Email"
+                      className="contact_form_Email input field"
+                      placeholder="Your Email" required="true" />
+
+                    <input type="Phone" id="contact_form_Phone"
+                      className="contact_form_Phone input field"
+                      placeholder="Your Phone number" required="true" />
+                  </div>
+                  <div className="contact_form_text mt-4">
+                    <textarea className="text field contact_form_message"  placeholder="Message"  id=""  cols="30" rows="10"></textarea>
+                  </div>
+                  <div className="contact_form_button mt-4">
+                    <button type="submit" className="button contact_submit_button"> 
+                        send Message
+                    </button>
+
+                  </div>
+                </form>
+
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+    </>
+
+  )
 
 }
 
