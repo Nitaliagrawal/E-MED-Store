9be2beab8816229716c69c317fdb8e0668435df5
Krupali2204 <71226293+Krupali2204@users.noreply.github.com>
2021-09-12 13:19:21 +0530

    done contact_us

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -9,7 +9,7 @@ const Contact = () => {
             <div className="col-lg-10 offset-lg-1 d-flex justify-content-between">
               {/* phone number*/}
               <div className="contact_info_item d-flex justify-content-start align-items-center">
-                <img src="" alt="phone" />
+                <img src="https://img.icons8.com/office/24/000000/iphone.png" alt="" />
                 <div className="contact_info_contect">
                   <div className="contact_info_title">
                     phone
@@ -20,7 +20,7 @@ const Contact = () => {
                 </div>
               </div>
               <div className="contact_info_item d-flex justify-content-start align-items-center">
-                <img src="" alt="Email" />
+                <img src="https://img.icons8.com/ultraviolet/24/000000/filled-message.png" alt="" />
                 <div className="contact_info_contect">
                   <div className="contact_info_title">
                     Email
@@ -31,7 +31,7 @@ const Contact = () => {
                 </div>
               </div>
               <div className="contact_info_item d-flex justify-content-start align-items-center">
-                <img src="" alt="Address" />
+                <img src="https://img.icons8.com/ultraviolet/24/000000/map-marker.png" alt="" />
                 <div className="contact_info_contect">
                   <div className="contact_info_title">
                     Address
@@ -61,23 +61,23 @@ const Contact = () => {
                 <form id="contact_form">
                   <div className="contact_form_name d-flex justify-content-between align-items-between">
                     <input type="text" id="contact_form_name"
-                      className="contact_form_name input field"
-                      placeholder="Your Name" required="true" />
+                      className="contact_form_name_input_field"
+                      placeholder="Your Name" required />
 
                     <input type="Email" id="contact_form_Email"
-                      className="contact_form_Email input field"
-                      placeholder="Your Email" required="true" />
+                      className="contact_form_name_input_field"
+                      placeholder="Your Email" required />
 
                     <input type="Phone" id="contact_form_Phone"
-                      className="contact_form_Phone input field"
-                      placeholder="Your Phone number" required="true" />
+                      className="contact_form_name_input_field"
+                      placeholder="Your Phone number" required />
                   </div>
                   <div className="contact_form_text mt-4">
-                    <textarea className="text field contact_form_message"  placeholder="Message"  id=""  cols="30" rows="10"></textarea>
+                    <textarea className="text_field contact_form_message" placeholder="Message" id="" cols="30" rows="10"></textarea>
                   </div>
-                  <div className="contact_form_button mt-4">
-                    <button type="submit" className="button contact_submit_button"> 
-                        send Message
+                  <div className="contact_form_button">
+                    <button type="submit" className="button contact_submit_button">
+                      send Message
                     </button>
 
                   </div>
