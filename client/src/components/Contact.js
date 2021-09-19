71a64cb0e5797d4aa8b7b6fbaa95e2c5c3db1c6f
Vaishnavi Patel <aishupatel150@gmail.com>
2021-09-19 22:20:18 +0530

    Card view added

---
+++ client/src/components/Contact.js
@@ -0,0 +1,101 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <>
+      <div className="contact_info">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-lg-10 offset-lg-1 d-flex justify-content-between">
+              {/* phone number*/}
+              <div className="contact_info_item d-flex justify-content-start align-items-center">
+                <img src="https://img.icons8.com/office/24/000000/iphone.png" alt="" />
+                <div className="contact_info_contect" style={{marginLeft:'10px'}}>
+                  <div className="contact_info_title">
+                    Phone
+                  </div>
+                  <div className="contact_info_title">
+                    +91 8849550613
+                  </div>
+                </div>
+              </div>
+              <div className="contact_info_item d-flex justify-content-start align-items-center">
+                <img src="https://img.icons8.com/ultraviolet/24/000000/filled-message.png" alt="" />
+                <div className="contact_info_contect" style={{marginLeft:'10px'}}>
+                  <div className="contact_info_title">
+                    Email
+                  </div>
+                  <div className="contact_info_title">
+                    abc@gmail.com
+                  </div>
+                </div>
+              </div>
+              <div className="contact_info_item d-flex justify-content-start align-items-center">
+                <img src="https://img.icons8.com/ultraviolet/24/000000/map-marker.png" alt="" />
+                <div className="contact_info_contect" style={{marginLeft:'10px'}}>
+                  <div className="contact_info_title">
+                    Address
+                  </div>
+                  <div className="contact_info_title">
+                    Gopal Vidyanagar, Tarsadi, Gujarat 394350
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+
+
+      <div className="contact_form">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-10 offset-lg-1">
+              <div className="contact_form_container py-5">
+                <div className="contact_form_title">
+                  Get in Touch
+                </div>
+                <form id="contact_form">
+                  <div className="contact_form_name d-flex justify-content-between align-items-between">
+                    <input type="text" id="contact_form_name"
+                      className="contact_form_name_input_field"
+                      placeholder="Your Name" required />
+
+                    <input type="Email" id="contact_form_Email"
+                      className="contact_form_name_input_field"
+                      placeholder="Your Email" required />
+
+                    <input type="Phone" id="contact_form_Phone"
+                      className="contact_form_name_input_field"
+                      placeholder="Your Phone number" required />
+                  </div>
+                  <div className="contact_form_text mt-4">
+                    <textarea className="text_field contact_form_message" placeholder="Message" id="" cols="30" rows="10"></textarea>
+                  </div>
+                  <div className="contact_form_button">
+                    <button type="submit" className="button contact_submit_button">
+                      send Message
+                    </button>
+
+                  </div>
+                </form>
+
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+    </>
+
+  )
+
+}
+
+export default Contact
---
+++ client/src/components/Signup.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import signup from "../images/signup.svg";
+
+const Signup = () => {
+    return (
+        <>
+            <section className="signup">
+                <div className="container mt-5">
+                    <div className="signup-content">
+                        <div className="row">
+                            <div className="col-md-6 col-sm-7">
+                                <div className="signup-form">
+                                    <h2 className="form-title">Sign Up</h2>
+                                    <form className="register-form" id="register-form">
+                                        <div className="form-group">
+                                            <label htmlFor="name">
+                                             <i className="zmdi zmdi-account material-icons-name"></i>
+                                            </label>
+                                            <input type="text" name="name" id="name" placeholder="Your Name" />
+                                        </div>
+                                        <div className="form-group">
+                                            <label htmlFor="email">
+                                                <i className="zmdi zmdi-email material-icons-name"></i>
+                                            </label>
+                                            <input type="text" name="email" id="email" placeholder="Your Email" />
+                                        </div>
+                                        <div className="form-group">
+                                            <label htmlFor="phone">
+                                                <i className="zmdi zmdi-phone-in-talk material-icons-name"></i>
+                                            </label>
+                                            <input type="number" name="phone" id="phone" placeholder="Your Phone number" />
+                                        </div>
+                                        <div className="form-group">
+                                            <label htmlFor="password">
+                                                <i className="zmdi zmdi-lock material-icons-name"></i>
+                                            </label>
+                                            <input type="text" name="password" id="password" placeholder="Your Password" />
+                                        </div>
+                                        <div className="form-group">
+                                            <label htmlFor="cpassword">
+                                                <i className="zmdi zmdi-lock material-icons-name"></i>
+                                            </label>
+                                            <input type="text" name="cpassword" id="cpassword" placeholder="Confirm Password" />
+                                        </div>
+                                        <div className="form-group">
+                                            <label htmlFor="address">
+                                                <i className="zmdi zmdi-pin material-icons-name"></i>
+                                            </label>
+                                            <textarea  id="address" rows="2" cols="44"  placeholder="Your address"></textarea>
+                                        </div>
+                                        <div className="form-group form-button">
+                                            <button type="button" class="btn btn-primary form-submit" name="signup" id="signup">Register</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                            <div className="col-md-6 col-sm-5">
+                                <div className="signup-image">
+                                    <figure>
+                                        <img src={signup} alt="registrationpic" className="img-fluid pr-5 mr-5 mt-5" />
+                                    </figure>
+                                    <NavLink to="/login" className="signup-image-link text-center mt-3">Already Register ?</NavLink>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Signup
